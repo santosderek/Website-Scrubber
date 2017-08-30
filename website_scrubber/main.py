0a27573d1ad4a017f118bd65f609104690eac7c1
Derek Santos <derekhalo@gmail.com>
2017-08-30 16:31:28 -0400

    Recursion Bug Fix
    Fixed a bug where the program would infinitely loop over the same domain
    if there was a "." link in the directory.

--- website_scrubber/main.py
+++ website_scrubber/main.py
@@ -7,13 +7,21 @@ from direct_downloader.direct_downloader import Download_Manager
 
 FINAL_RECURRSION_LEVEL = 0
 
+# TODO: Logger - Possibily 3 different files "Skipped.txt", "Failed.txt"
+# TODO: Download Percentage needs to be displayed
+
 
 def parse_arguments():
     parser = ArgumentParser(description='Scrub a directory.')
     parser.add_argument('url', metavar='url', type=str,
                         nargs='*', help='Automatically detect urls')
+
     parser.add_argument('--level', '-l', metavar='level',
                         type=int, nargs='?', help='Levels of recursion.')
+
+    parser.add_argument('--threads', '-t', metavar='threads',
+                        type=int, nargs='?', help='Number of threads to use.')
+
     parser.add_argument('--folder', '-f', metavar='folder',
                         type=str, nargs='?', help='Path of folder to download to.')
 
@@ -26,6 +34,42 @@ def return_main_site_text(url):
         return site.text
     else:
         return None
+
+
+def purge_negative_links(list_of_urls: list, url: str):
+    # Take out parent link and url
+    while url in list_of_urls:
+        list_of_urls.remove(url)
+
+    while url + '/' in list_of_urls:
+        list_of_urls.remove(url + '/')
+
+    while url + '.' in list_of_urls:
+        list_of_urls.remove(url + '.')
+
+    while url + '..' in list_of_urls:
+        list_of_urls.remove(url + '..')
+
+    while url + './' in list_of_urls:
+        list_of_urls.remove(url + './')
+
+    while url + '../' in list_of_urls:
+        list_of_urls.remove(url + '../')
+
+    # Appache Parent Redirects
+    while url + '?C=D;O=A' in list_of_urls:
+        list_of_urls.remove(url + '?C=D;O=A')
+
+    while url + '?C=N;O=D' in list_of_urls:
+        list_of_urls.remove(url + '?C=N;O=D')
+
+    while url + '?C=S;O=A' in list_of_urls:
+        list_of_urls.remove(url + '?C=S;O=A')
+
+    while url + '?C=M;O=A' in list_of_urls:
+        list_of_urls.remove(url + '?C=M;O=A')
+
+    return list_of_urls
 
 
 def return_file_and_folder_links(html, url):
@@ -47,20 +91,8 @@ def return_file_and_folder_links(html, url):
     absolute_directories = [
         url + link if not re.match('^(https://|http://)', link) else url for link in relative_directories]
 
-    # Take out parent link and url
-    # ... File links
-    while url in absoulute_file_links:
-        absoulute_file_links.remove(url)
-
-    while url + '../' in absoulute_file_links:
-        absoulute_file_links.remove(url + '../')
-
-    # ... Directories
-    while url in absolute_directories:
-        absolute_directories.remove(url)
-
-    while url + '../' in absolute_directories:
-        absolute_directories.remove(url + '../')
+    absoulute_file_links = purge_negative_links(absoulute_file_links, url)
+    absolute_directories = purge_negative_links(absolute_directories, url)
 
     return absolute_directories, absoulute_file_links
 
@@ -85,12 +117,12 @@ def download(url: str, path: str, current_level=0, threads=3):
 
         # Change directory into the directory where the downloads should be
         os.chdir(path)
+        print('Moved to:', path)
 
         absoulute_directories, absoulute_file_links = return_file_and_folder_links(
             html, url)
 
         # Send to download Manager
-        print('Moved to:', path)
         manager = Download_Manager(absoulute_file_links, threads, '.')
         manager.start()
 
@@ -99,6 +131,7 @@ def download(url: str, path: str, current_level=0, threads=3):
         #print('Final Level:', FINAL_RECURRSION_LEVEL)
         if current_level != FINAL_RECURRSION_LEVEL:
             for next_url in absoulute_directories:
+                print('----------next url:', next_url)
                 start = next_url.rfind('/', 0, len(next_url) - 1)
                 download(next_url,
                          '.' + next_url[start:],
@@ -129,6 +162,14 @@ def main():
     else:
         folder = arguments.folder
 
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    if arguments.threads is None:
+        threads = 3
+    else:
+        threads = arguments.threads
+
     os.chdir(folder)
 
     # If no url, exit
@@ -137,7 +178,7 @@ def main():
 
     # Download all urls in arguments
     for url in arguments.url:
-        download(url, folder)
+        download(url, folder, threads=threads)
 
 
 if __name__ == '__main__':
